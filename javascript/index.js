512626d5a3e05906f022b08c78cd79d98b02b219
Angelikikark <167430380+Angelikikark@users.noreply.github.com>
2024-11-03 21:06:47 +0000

    added a comment

--- javascript/index.js
+++ javascript/index.js
@@ -30,7 +30,7 @@ function updateCity(event) {
   let cityTimeZone = event.target.value;
   if (cityTimeZone === "current") {
     cityTimeZone = moment.tz.guess();
-  }
+  } //JS Time Zones in the current location
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
